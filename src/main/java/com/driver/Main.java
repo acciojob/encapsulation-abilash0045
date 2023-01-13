edf7b32f1e99bb81de55f495528f21ad3c95f5c7
Abilash S L <abilash0045@gmail.com>
2023-01-13 13:33:40 +0530

    Add soln1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Abilash");
+        String name = obj.getName();
+        System.out.println(name);
+        // name has private access in com.driver.RWOnly
+    }
 }
